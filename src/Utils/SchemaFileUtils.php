d6abfe34bfaf8825b80ea66c5a57d2eb4009193a
Mikołaj Salamak <m.salamak@colect-house.pl>
2022-06-28 10:42:28 +0200

    Poprawiono pobieranie URL

--- src/Utils/SchemaFileUtils.php
+++ src/Utils/SchemaFileUtils.php
@@ -11,13 +11,15 @@ class SchemaFileUtils
 {
     public static function cacheFile($namespace, $file): string
     {
-        $file = urldecode($file);
-        $file = explode('.xsd', $file, 2)[0].'.xsd';
+        $file = self::sanitizeUrl(urldecode($file));
+        if(str_contains($file, '.xsd')){
+            $filename = explode('.xsd', $file, 2)[0].'.xsd';
+        } else {
+            $filename = $file.'.xsd';
+        }
         $cached = self::getCachedFile($namespace, $file);
         if(!$cached) {
-            $fileCont = file_get_contents($file);
-            $cached = 'StormCode/SchemaReader/'.str_replace([':','/'], ['','_'], $file);
-            Storage::put($cached, $fileCont);
+            $cached = self::downloadFile($file, 'StormCode/SchemaReader/'.str_replace([':','/'], ['','_'], $filename));
             SchemaCache::create([
                 'schema_namespace' => $namespace,
                 'schema_download_uri' => $file,
@@ -26,6 +28,31 @@ class SchemaFileUtils
         }
 
         return str_replace('\\', '/', storage_path('app/'.$cached));
+    }
+
+    private static function sanitizeUrl($url)
+    {
+        return str_replace(
+            [
+                ' ',
+            ], 
+            [
+                '',
+            ], 
+            $url
+        );
+    }
+
+    private static function downloadFile($fileUrl, $filepath) 
+    {
+        $out = file_get_contents($fileUrl);
+        $finfo = new \finfo(FILEINFO_MIME);
+        if(!str_contains($finfo->buffer($out), 'text/xml')) {
+            throw new \Exception('Zły mimetype pliku schematu');
+        }
+        Storage::put($filepath, $out);
+
+        return $filepath;
     }
 
     public static function getCachedFile($namespace, $file): string | bool
